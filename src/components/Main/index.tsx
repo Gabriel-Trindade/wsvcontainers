bcc8ccb7ed3d02f0a04160632b8feb15980af92f
Gabriel Trindade <83348861+Gabriel-Trindade@users.noreply.github.com>
2024-02-03 16:53:49 -0300

    Initial commit

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,10 @@
+import * as styles from './styles'
+
+const Main = () => {
+  return (
+    <styles.wrapper>
+      <h1>React Avançado</h1>
+    </styles.wrapper>
+  )
+}
+export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,12 @@
+'use client'
+
+import styled from 'styled-components';
+
+export const wrapper = styled.main`
+    display: flex;
+    background-color: #f5f5f5;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+`
